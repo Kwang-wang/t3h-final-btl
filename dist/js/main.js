5f75ed568e874bafb7f23c2e50ce1ea4f7bb9639
Quang Truong <kwang@gmail.com>
2023-04-12 13:59:17 +0700

    add undone shop-cart property

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,12 @@
 var input_space_1 =document.querySelectorAll("#input_space");
 var text_space =document.querySelectorAll("#text_a");
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('btn_delete')) {
+      var parent = event.target.closest('.danhmucocv');
+      parent.style.display = 'none';
+    }
+  });
+  
 function anchukhiclickvaoinput(input_space,text)
 {
     if(event.target !== input_space && input_space.value == false)
@@ -19,6 +26,12 @@ document.addEventListener('click',function(event){
 
     }
 })
+function xuligiasanpham(giasp){
+    var price = parseInt(giasp.replace(/,/g, ""));
+    var priceFormatted = price.toLocaleString("vi-VN");
+    return priceFormatted;
+
+}
 
 $(document).ready(function(){
     
@@ -51,5 +64,31 @@ $(document).ready(function(){
         $(this).addClass('mouseleave')
     })
     
+
+    
+
+    $('.add_to_cart').click(function()
+    {   var container = $(this).closest(".product-list-element");
+        var price_div = container.find('div');
+        var name_sp = container.find('h3').text();
+        var price_sp = price_div.find('.cr-price').text();
+        var price_sp_solved = xuligiasanpham(price_sp)
+        var imgSrc = container.find('img').attr('src');
+       
+        var danhmuc_ocv =  $('<div/>', { class: 'danhmucocv', html :'<div class = "name_ocv">'+name_sp+'</div><div class = "price_ocv">' + price_sp_solved +"đ"+ '</div>' });
+        var img_danhmuc_ocv = $('<div/>',{class : 'img_ocv',html : '<img src='+'"'+ imgSrc +'"'+ '</img> '})
+        var btn_confirm = $('<div/>',{class : 'btn_confirm_a'})
+        danhmuc_ocv.append(btn_confirm)
+        $('.offcanvas-body').append(danhmuc_ocv)
+        danhmuc_ocv.append(img_danhmuc_ocv)
+        danhmuc_ocv.append('<button class = "btn_delete">Xóa</button>')
+        
+
+    })
+    
     
 });
+$(document).on('click', '.btn_confirm_a', function() {
+    $(this).toggleClass('chosen');
+    $(this).closest('.danhmucocv').toggleClass('chosen2')
+  });
