8d2490edb2f36167e777bca42bfc492982057656
Quang Truong <kwang@gmail.com>
2023-04-17 13:28:25 +0700

    done homepage

--- dist/js/main.js
+++ dist/js/main.js
@@ -147,7 +147,7 @@ $(document).ready(function(){
             0:{
                 items:1
             },
-            600:{
+            800:{
                 items:2
             },
             1000:{
